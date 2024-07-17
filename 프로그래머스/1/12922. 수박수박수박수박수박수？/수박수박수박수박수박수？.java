79c65d209f98d95508ecab3314d60b7321f2c210
quokkavely <165968530+quokkavely@users.noreply.github.com>
2024-07-17 22:52:30 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.73 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int n) {
+ 
+        
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i = 0; i < n ; i++ ){
+            if(i%2!=0){
+                answer.append("박");
+            }else{
+                answer.append("수");
+            }
+        }
+        
+        return answer.toString();
+    }
+}
