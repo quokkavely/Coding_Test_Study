963e7ce8be876cd4574c3d4b369e21f949f375a0
quokkavely <165968530+quokkavely@users.noreply.github.com>
2024-07-20 10:20:02 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.10 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int maxCount = 0;
+        int zeroCount = 0;
+        
+        ArrayList<Integer> win = new ArrayList<>();
+            for(int i : win_nums) {
+                win.add(i);
+            }  
+        
+        for(int i = 0; i < lottos.length ; i++) {
+            if(win.contains(lottos[i])) {
+                maxCount ++;
+            }
+            
+            if(lottos[i]==0) {
+                zeroCount ++;
+            }
+        }
+        
+        answer[0] = 6-maxCount-zeroCount+1;
+        answer[1] = 6-maxCount+1;
+        
+        if( answer[0] == 7 ) {
+            answer[0] = 6;
+        }
+        
+        if(answer[1] == 7) {
+            answer[1] = 6 ;
+        }
+        
+        return answer;
+
+    }
+}
