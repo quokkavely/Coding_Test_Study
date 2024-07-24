ed899c94bf031f8389523a235ab24764fdfe7e6f
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-07-24 22:46:19 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.76 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+       String[] answer1 = intToBinary(n, arr1);
+       String[] answer2 = intToBinary(n, arr2);
+       
+        
+        for(int i = 0; i < n ; i++) {
+             String str = "";
+            for(int j =0 ; j < n ; j++) {
+            if(answer1[i].charAt(j) == '1' || answer2[i].charAt(j) == '1' ) {
+                str += "#";
+            }else{
+                str+=" ";
+            }
+        }
+            answer[i] = str;
+        }
+        
+        return answer;
+    }
+    
+        public String[] intToBinary(int n, int [] arr) {
+            String answer[] = new String[n];
+            
+                for(int i =0; i < n ;i++) {
+                
+                String back = Integer.toBinaryString(arr[i]);
+                String front = "";
+                
+                if(back.length() < n) {
+                    front = "0".repeat(n-back.length());
+                }
+                    answer[i] = front + back;
+            }
+            return answer;
+        }
+        
+    
+}
