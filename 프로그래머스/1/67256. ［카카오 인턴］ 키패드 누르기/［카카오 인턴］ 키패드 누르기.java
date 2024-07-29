26ce42d1dae93754a411c8d9f9dce2cee50553f4
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-07-29 23:56:13 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 6.56 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        Map <String, Integer> map = new HashMap<>();
+            map.put("L", 10);
+            map.put("R", 12);
+        
+        for(int number : numbers) {
+            if(number == 0) number = 11;
+        
+            
+            if(number % 3 == 0 && number != 0 ) {
+                map.put("R", number);
+                answer += "R";
+                
+            } else if(number % 3 == 1 ) {
+                map.put("L", number);
+                answer+= "L";
+            
+            } else {
+                
+                int leftDiff = clickCenter(map.get("L"), number);
+                int rightDiff = clickCenter(map.get("R"), number);
+                
+                if(leftDiff > rightDiff ) {
+                    map.put("R", number);
+                    answer += "R";
+                
+                } else if(leftDiff < rightDiff) {
+                    
+                    map.put("L", number);
+                    answer+= "L";
+             
+                } else {
+                        if (hand.equals("right")) {
+                        map.put("R", number);
+                        answer += "R";
+                            
+                    } else {
+                       
+                        map.put("L", number);
+                        answer += "L";
+                    }
+                }
+            }
+        }
+            
+            return answer;
+            
+        }
+    
+        
+        public static int clickCenter(int previousKey, int currentKey) {
+            
+            if (previousKey == 0) previousKey = 11; 
+            if (currentKey == 0) currentKey = 11; 
+
+            int fromX = (previousKey - 1) / 3;
+            int fromY = (previousKey - 1) % 3;
+            int toX = (currentKey - 1) / 3;
+            int toY = (currentKey - 1) % 3;
+
+            return Math.abs(fromX - toX) + Math.abs(fromY - toY);
+        }
+    
+ }
