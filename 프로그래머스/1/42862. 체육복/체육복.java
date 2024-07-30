105279933a4efb50d2fe4030855ab6131b3325c0
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-07-30 17:24:56 +0900

    [level 1] Title: 체육복, Time: 0.64 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,34 +3,37 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // Sort arrays for consistent processing
+   
         Arrays.sort(lost);
         Arrays.sort(reserve);
+        int count = n;
         
-        // Convert arrays to sets for easy removal
-        Set<Integer> lostSet = Arrays.stream(lost).boxed().collect(Collectors.toSet());
-        Set<Integer> reserveSet = Arrays.stream(reserve).boxed().collect(Collectors.toSet());
-
-        // Handle students who are both in lost and reserve
-        Set<Integer> intersection = new HashSet<>(lostSet);
-        intersection.retainAll(reserveSet);
-        lostSet.removeAll(intersection);
-        reserveSet.removeAll(intersection);
-
-        // Start with the total number of students who can participate
-        int answer = n - lostSet.size();
-
-        // Try to lend reserve uniforms to lost students
-        for (int r : reserveSet) {
-            if (lostSet.contains(r - 1)) {
-                lostSet.remove(r - 1);
-                answer++;
-            } else if (lostSet.contains(r + 1)) {
-                lostSet.remove(r + 1);
-                answer++;
+        HashSet<Integer> reserveSet = new HashSet<>();
+        HashSet<Integer> lostSet = new HashSet<>();
+         
+        for (int i : reserve ) {
+            reserveSet.add(i);
+        }
+        
+        for (int i : lost) {
+            if (reserveSet.contains(i)) {
+                reserveSet.remove(i);
+            } else {
+                lostSet.add(i);
             }
         }
-
-        return answer;
+        
+        for (int i : lost) {
+            if (lostSet.contains(i)) {
+                if (reserveSet.contains(i-1)) {
+                    reserveSet.remove(i-1);
+                } else if(reserveSet.contains(i+1)) {
+                    reserveSet.remove(i+1);
+                } else {
+                    count--;
+                }
+            }
+        }
+        return count;
     }
 }
